845b038adad7f9421e9ec90187c46ae6214ecd27
Gabrielle <gabriellesantisilva@gmail.com>
2026-01-28 08:17:08 -0300

    First commit

---
+++ src/main/java/com/playwright/PlaywrightProject/PlaywrightProjectApplication.java
@@ -0,0 +1,13 @@
+package com.playwright.PlaywrightProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywrightProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywrightProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playwright/PlaywrightProject/PlaywrightProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playwright.PlaywrightProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaywrightProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
